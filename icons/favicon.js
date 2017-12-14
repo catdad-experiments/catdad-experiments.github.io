66c2bffdb3d08a608cb3911114d842ab6c02148f
Kiril Vatev <vatev.1@gmail.com>
2017-12-13 20:21:19 -0500

    adding favicon, inject script

---
+++ icons/favicon.js
@@ -0,0 +1,39 @@
+/* jshint browser: true */
+
+//  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+//  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+//  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+//  <link rel="manifest" href="/manifest.json">
+//  <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#4f9aa3">
+//  <meta name="theme-color" content="#4f9aa3">
+
+(function () {
+  function icon(url, size) {
+    var type = /\.svg$/.test(url) ? 'image/svg+xml' : 'image/png';
+
+    var link = document.createElement('link');
+    link.type = type;
+    link.rel = 'icon';
+    link.sizes = size;
+    link.href = url;
+
+    return link;
+  }
+
+  function themeColor(color) {
+    var meta = document.createElement('meta');
+    meta.name = 'theme-color';
+    meta.content = color;
+
+    return meta;
+  }
+
+  var head = document.querySelector('head');
+  var fragment = document.createDocumentFragment();
+
+  fragment.appendChild(icon('/icons/favicon.png'), '92x92');
+  fragment.appendChild(icon('/icons/favicon.svg'), 'any');
+  fragment.appendChild(themeColor('#4f9aa3'));
+
+  head.appendChild(fragment);
+}());
